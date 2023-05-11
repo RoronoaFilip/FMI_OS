858ed6605beac435b1ef255f12fcf748e6f604fc
Filip Filchev <fkfilchev@gmail.com>
2023-05-11 22:34:39 +0300

    Update Task 59

--- exercise/book/2/59-2017IN01/main.c
+++ exercise/book/2/59-2017IN01/main.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 struct pair {
 	uint16_t offset;
@@ -11,6 +13,8 @@ struct pair {
 };
 
 int main(int argc, char** argv) {
+	setbuf(stdout, NULL);
+
 	if (argc != 5)
 		errx(2, "ERROR: params count");
 
@@ -23,12 +27,54 @@ int main(int argc, char** argv) {
 		err(2, "ERROR: opening file for read: %s", argv[2]);
 
 	int fd3;
-	if((fd3 = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR)) == -1)
+	if((fd3 = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR + S_IWUSR)) == -1)
 		err(2, "ERROR: opening file for write: %s", argv[3]);
 	
 	int fd4;
-	if((fd4 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR)) == -1)
+	if((fd4 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR + S_IWUSR)) == -1)
 		err(2, "ERROR: opening file for write: %s", argv[4]);
+
+	struct stat s;
+	if(fstat(fd2, &s) == -1)
+		err(3, "ERROR: getting file size");
+
+	struct pair p;
+	if(s.st_size % sizeof(p) != 0)
+		err(3, "File size not correct: %s", argv[2]);
+	
+	int bytesRead=0;
+	while((bytesRead = read(fd2, &p, sizeof(p))) > 0) {
+		p.offset--;
+		if(lseek(fd1, p.offset, SEEK_SET) == -1)
+			err(4, "ERROR: lseek in file: %s", argv[1]);
+		
+		char buf;
+		if(read(fd1, &buf, sizeof(buf)) == -1)
+			err(5, "ERROR: reading from file: %s", argv[1]);
+
+		if(buf < 'A' || buf > 'Z')
+			continue;
+
+		if(write(fd3, &buf, sizeof(buf)) == -1)
+			err(6, "ERROR: writing to file: %s", argv[3]);
+		for(int i = 1; i < p.len; ++i) {
+			if(read(fd1, &buf, sizeof(buf)) == -1)
+				err(5, "ERROR: reading from file: %s", argv[1]);
+			if(write(fd3, &buf, sizeof(buf)) == -1)
+				err(6, "ERROR: writing to file: %s", argv[3]);
+		}
+
+		if(write(fd4, &p, sizeof(p)) == -1)
+			err(7, "ERROR: writing to file: %s", argv[4]);
+	}
+
+	if(bytesRead != 0)
+		err(4, "ERROR: reading from file: %s", argv[1]);
+
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	close(fd4);
 
 	exit(0);
 }
