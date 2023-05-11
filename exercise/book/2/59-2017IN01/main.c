e343de6bb90aa9cfc21eafbe64e40d103f287fdd
Filip Filchev <fkfilchev@gmail.com>
2023-05-11 10:24:29 +0300

    Add Task 59

---
+++ exercise/book/2/59-2017IN01/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <err.h>
+#include <stdint.h>
+#include <sys/stat.h>
+
+struct pair {
+	uint16_t offset;
+	uint8_t len;
+	uint8_t res;
+};
+
+int main(int argc, char** argv) {
+	if (argc != 5)
+		errx(2, "ERROR: params count");
+
+
+
+	exit(0);
+}
