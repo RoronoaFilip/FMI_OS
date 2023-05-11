1a63cb698ac0c4058431e4a5b2d69fbb35ec8f29
Filip Filchev <fkfilchev@gmail.com>
2023-05-11 11:41:08 +0300

    Update Tasks

--- exercise/book/2/56-2016SE01/main.c
+++ exercise/book/2/56-2016SE01/main.c
@@ -9,9 +9,9 @@ int main(int argc, char** argv) {
 	if (argc != 2) 
 		errx(2, "Number of params must be 1");
 
-	u_int32_t arr[256];
-	for(int i = 0; i < 256; ++i)
-		arr[i] = 0;
+	u_int32_t arr[256] = {0};
+//	for(int i = 0; i < 256; ++i)
+//		arr[i] = 0;
 
 	int fd1;
 	if((fd1 = open(argv[1], O_RDWR)) == -1)
@@ -19,15 +19,13 @@ int main(int argc, char** argv) {
 
 	int bytesRead = 0;
 	uint8_t c = 0;
-	while((bytesRead = read(fd1, &c, sizeof(c))) > 0) {
+	while((bytesRead = read(fd1, &c, sizeof(c))) > 0)
 		arr[c] += 1;	
-	}
 
 	if(bytesRead != 0) 
 		errx(2, "Error while reading file: %s", argv[1]);
 
-	int lseekCheck = lseek(fd1, 0, SEEK_SET);
-	if(lseekCheck == -1)
+	if(lseek(fd1, 0, SEEK_SET))
 		errx(2, "Error returning lseek to begin of file %s", argv[1]);
 
 	for(uint16_t i = 0; i < 256; ++i) {
