70e607f0428c248790783c557b29a50af9c29b7b
Филип Филчев <s0600041@astero.openfmi.net>
2023-05-15 18:06:22 +0300

    Add Task 64

---
+++ exercise/book/2/64-2018SE01/main.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <string.h>
+#include <err.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+int charIndex(char c, char* str) {
+	for(int i = 0; str[i] != '\0'; ++i)
+		if(str[i] == c)
+			return i;
+
+	return -1;
+}
+
+int main(int argc, char** argv) {
+	bool S = false;
+	bool D = false;
+
+	if(argc != 3)
+		errx(1, "ERROR: params count");
+
+	if(strcmp("-s", argv[1]) == 0) {
+		S = true;
+	} else if(strcmp("-d", argv[1]) == 0) {
+		D = true;
+	} else if(strlen(argv[1]) != strlen(argv[2])) {
+		errx(1, "ERROR: first and second arguments are different lengths");
+	}
+
+	int bytes;
+	char buf;
+	char prev = '\0';
+
+	while((bytes = read(0, &buf, sizeof(buf))) > 0) {
+		if(S) {
+			if(prev == '\0' || (charIndex(buf, argv[2]) != -1 && buf != prev)){
+				if(write(1, &buf, sizeof(buf)) == -1)
+					err(3, "ERROR: writing to stdout with -s");
+			}
+
+			prev = buf;
+		} else if(D) {
+			if(charIndex(buf, argv[2]) == -1)
+				if(write(1, &buf, sizeof(buf)) == -1)
+					err(3, "ERROR: writing to stdout with -d");
+		} else {
+			int index = charIndex(buf, argv[1]);
+
+			if(index != -1)
+				buf = argv[2][index];
+
+			if(write(1, &buf, sizeof(buf)) == -1)
+				err(3, "ERROR: writing to stdout with no options");
+		}	
+	}
+	if(bytes != 0)
+		err(2, "ERROR: reading from stdin");
+
+	if(S)
+		printf("\n");
+
+	return 0;
+}
