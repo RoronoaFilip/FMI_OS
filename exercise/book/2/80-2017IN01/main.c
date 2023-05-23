b6d7478f8c6cf7dc1b6f20b81b70416535389f90
Filip Filchev <fkfilchev@gmail.com>
2023-05-23 22:48:17 +0300

    Update Task 80

--- exercise/book/2/80-2017IN01/main.c
+++ exercise/book/2/80-2017IN01/main.c
@@ -5,85 +5,105 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-
 	int pf[2];
-
 	if(pipe(pf) == -1)
-		errx(1, "ERROR: using pipe()");
+		errx(1, "ERROR: calling pipe() for pf");
 
-	pid_t pid = fork();
-
-	if(pid == 0) {
-		printf("CAT");
+	pid_t pid;
+	if((pid = fork()) == 0) {
 		close(pf[0]);
 		
-		close(1);
-		if(dup2(pf[1], 1) == -1)
-			errx(2, "Redirecting stdout in child");
+		dup2(pf[1], 1);
 		close(pf[1]);
 
 		execlp("cat", "cat", "/etc/passwd", (char*)NULL);
 
-		errx(3, "ERROR: calling cat in child");
-		return 0;
+		err(2, "ERROR: running cat");
+		return 1;
 	}
-	waitpid(pid, NULL, 0);
 
-	pid = fork();
-	if(pid == 0) {
-		close(0); close(1);
-		if(dup2(pf[0], 0) == -1 || dup2(pf[1], 1) == -1)
-			errx(4, "ERROR: redirecting stdout or stdin in parent");
+	close(pf[1]);
 
-		close(pf[0]); close(pf[1]);
+	int pf2[2];
+	if(pipe(pf2) == -1)
+		errx(1, "ERROR: calling pipe() for pf2");
+
+	if((pid = fork()) == 0) {
+		close(pf2[0]);
+		
+		dup2(pf[0], 0);
+		close(pf[0]);
+		
+		dup2(pf2[1], 1);
+		close(pf2[1]);
 
 		execlp("cut", "cut", "-d", ":", "-f", "7", (char*)NULL);
 
-		errx(5, "ERROR: calling cut in child");
+		err(3, "ERROR: running  cut -d : -f 7");
+		return 3;
 	}
-	
-	waitpid(pid, NULL, 0);	
 
-	pid = fork();
-	if(pid == 0) {
-		close(0); close(1);
-		if(dup2(pf[0], 0) == -1 || dup2(pf[1], 1) == -1)
-			errx(4, "ERROR: redirecting stdout or stdin in child");
-		close(pf[0]); close(pf[1]);
+	close(pf[0]);
+	close(pf2[1]);
+
+	int pf3[2];
+	if(pipe(pf3) == -1)
+		errx(1, "ERROR: calling pipe() for pf3");
+
+	if((pid = fork())) {
+		close(pf3[0]);
+
+		dup2(pf2[0], 0);
+		close(pf2[0]);
+
+		dup2(pf3[1], 1);
+		close(pf3[1]);
 
 		execlp("sort", "sort", (char*)NULL);
 
-		errx(5, "ERROR: calling sort in child");
+		err(4, "ERROR: running sort1");
+		return 4;
 	}
-	
-	waitpid(pid, NULL, 0);
 
-	pid = fork();
-	if(pid == 0) {
-		close(0); close(1);
-		if(dup2(pf[0], 0) == -1 || dup2(pf[1], 1) == -1)
-			errx(6, "ERROR: redirecting stdout or stdin in child");
-		close(pf[0]); close(pf[1]);
+	close(pf2[0]);
+	close(pf3[1]);
+
+	int pf4[2];
+	if(pipe(pf4) == -1)
+		errx(1, "ERROR: calling pipe() for pf4");
+
+	if((pid = fork()) == 0) {
+		close(pf4[0]);
+
+		dup2(pf3[0], 0);
+		close(pf3[0]);
+
+		dup2(pf4[1], 1);
+		close(pf4[1]);
 
 		execlp("uniq", "uniq", "-c", (char*)NULL);
 
-		errx(5, "ERROR: calling uniq -c in child");
-		return 0;
+		err(5, "ERROR: running uniq -c");
+		return 5;
 	}
 
-	waitpid(pid, NULL, 0);
+	close(pf3[0]);
+	close(pf4[1]);
 
-	pid = fork();
-	if(pid == 0) {
-		close(0);
-		if(dup2(pf[0], 0) == -1)
-			errx(7, "ERROR: redirecting stdout or stdin in child");
-		close(pf[0]); close(pf[1]);
+	if((pid = fork()) == 0) {
+		dup2(pf4[0], 0);
+		close(pf4[0]);
 
 		execlp("sort", "sort", (char*)NULL);
 
-		errx(5, "ERROR: calling sort in child 2nd time");
+		err(6, "ERROR: running sort2");
+		return 6;
 	}
+
+	close(pf4[0]);
+
+	int status;
+	while(wait(&status) > 0) {}
 
 	return 0;
 }
