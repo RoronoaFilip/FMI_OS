eed0be1ffe30c660afb1886bc193b7d4bd66f7bf
Filip Filchev <fkfilchev@gmail.com>
2023-06-07 08:59:34 +0300

    Add Task 85

---
+++ exercise/book/2/85-2020SE02/main.c
@@ -0,0 +1,14 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv) {
+	
+	if(argc != 3)
+		errx(1, "ERROR: params count");
+
+	int fd1;
+
+	return 0;
+}
