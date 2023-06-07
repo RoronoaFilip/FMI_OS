e130ac08359e8583d3c515d4f870b13cd75c851a
Филип Филчев <s0600041@astero.openfmi.net>
2023-06-08 00:30:37 +0300

    Finish Task 85

--- exercise/book/2/85-2020SE02/main.c
+++ exercise/book/2/85-2020SE02/main.c
@@ -7,6 +7,7 @@
 uint8_t ignore = 0x55;
 uint8_t escape = 0x7D;
 uint8_t xor = 0x20;
+
 int main(int argc, char** argv) {
 	
 	if(argc != 3)
@@ -26,8 +27,8 @@ int main(int argc, char** argv) {
 		dup2(pd[1], 1);
 		close(pd[1]);
 
-		if(execlp("xxd", "xxd", "-p", argv[1], (char*)NULL) == -1)
-			err(4, "ERROR: running xxd -p in child");
+		if(execlp("cat", "cat",  argv[1], (char*)NULL) == -1)
+			err(4, "ERROR: running cat in child");
 	}
 
 	close(pd[1]);
@@ -43,9 +44,11 @@ int main(int argc, char** argv) {
 			continue;
 
 		if(buf == escape) {
-			bytes = read(pd[0], &buf, sizeof(buf);
+			bytes = read(pd[0], &buf, sizeof(buf));
+
 			if(bytes < 0)
 				err(7, "ERROR: reading from file 1; %s", argv[1]);
+
 			buf = buf ^ xor;
 		}
 
