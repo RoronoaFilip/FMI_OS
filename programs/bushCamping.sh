86a9ddbc1e24f81924a125ab28e52c12a6148caa
Filip Filchev <fkfilchev@gmail.com>
2023-06-25 11:55:39 +0300

    Move Folders and add pdf

--- programs/bushCamping.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while ! who | egrep "$1"; do
-	sleep 5
-done
-
-echo "$1 is online"
-
-./spam.sh $(who | egrep "$1" | tr -s ' ' |  cut -d ' ' -f 1) &
--- programs/spam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-	pts=$(who | egrep "$1" | tr -s ' ' | cut -d ' ' -f 2)
-
-    echo "You have been hacked!" | write $1 $pts 1>/dev/null 2>&1
-
-    sleep 2
-done
